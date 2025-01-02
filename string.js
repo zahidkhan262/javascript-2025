ed37213a0c051d16a7ae62c59e50eff35be4726d
乙多れ个Ð Kれ多ภ <zahidkhan262@otsusawares-MacBook-Air.local>
2025-01-02 13:17:19 +0530

    optimize

--- string.js
+++ string.js
@@ -1,12 +1,10 @@
 const reverseStr = (str)=>{
     str = str.trim();
-  let newStr="";
-
-  for(let i=str.length-1; i>=0; i--){
-    newStr +=str[i];
-  }
-
-   return newStr
+    let newStr="";
+    for(let i=str.length-1; i>=0; i--){
+        newStr +=str[i];
+    }
+    return newStr
 }
 
 console.log(reverseStr("zahid"))
